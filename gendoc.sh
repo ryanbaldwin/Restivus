26d4f2d24fa9487b1fbdf392a8780128c9f010bd
ryanbaldwin <ryanbaldwin@gmail.com>
2017-09-29 17:32:03 -0400

    removed docs. created script to generate docs.

---
+++ gendoc.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+jazzy \
+    --source-directory .\
+    --readme ./README.md \
+    -a 'Ryan Baldwin' \
+    -u 'https://github.com/ryanbaldwin' \
+    -m 'Restivus' \
+    -g 'https://github.com/ryanbaldwin/Restivus' \
+    -o $1
+
