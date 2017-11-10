4f30ad125ea7caa95f6e7f23808e3ea5ccc16e42
Ryan Baldwin <ryanbaldwin@gmail.com>
2017-11-10 16:34:31 -0500

    updated docstrings in Interceptable

--- Restivus/Interceptable.swift
+++ Restivus/Interceptable.swift
@@ -1,5 +1,5 @@
 //
-//  Authenticating.swift
+//  Interceptable.swift
 //  Restivus
 //
 //  Created by Ryan Baldwin on 2017-08-28.
@@ -8,14 +8,12 @@
 
 import Foundation
 
-/// Provides pre-submission hook which allows a Restable to mutate the HTTPURLRequest
-/// such that the request will pass authentication with a secured endpoint.
+/// Provides pre-submission hook which allows a Restable to mutate the HTTPURLRequest.
 /// For example, adding a token in a header.
 public protocol Interceptable {
-    /// Signs the provided URLRequest by whatever means is required for authenticating
-    /// the request against its destination.
+    /// Intercepts a URLRequest and allows the handler to mutate it to its heart content
     ///
-    /// - Parameter request: The URLRequest requiring authentication
-    /// - Returns: A signed version of the provided `request`
+    /// - Parameter request: The intercepted URLRequest
+    /// - Returns: A (possibly) mutated version of the provided `request`
     func intercept(request: URLRequest) -> URLRequest
 }
