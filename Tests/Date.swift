d6afbe4b429e9d802fe0d1171fc084e83338e45c
Adrian de Almeida <adealmeida@ehealthinnovation.org>
2018-06-14 15:10:52 -0400

    Added some small cleanup.

--- Tests/Date.swift
+++ Tests/Date.swift
@@ -7,8 +7,6 @@
 //
 
 import Foundation
-import Quick
-import Nimble
 @testable import Restivus
 
 struct DateResponse: Decodable {
--- Tests/GettableSpec.swift
+++ Tests/GettableSpec.swift
@@ -36,33 +36,30 @@ class GettableSpec: QuickSpec {
                 let currentDateString = "2018-06-12T20:00:00.000-04:00"
                 let currentDate = gettable.dateFormatter().date(from: currentDateString)!
                 
-                expect {
-                    let data = """
-                    { "date": "\(currentDateString)" }
-                    """.data(using: .utf8)!
-                    
-                    var dateResponse: DateResponse? = nil
-                    
-                    // NOTE: We use a dummy url response here as the `dataTaskCompletionHandler` needs it.
-                    // As long as this method gets a success statusCode (ie. 200) from an HTTPURLResponse, it will proceed to decode the data passed in.
-                    // We want to verify that it is decoding it according to the `dateDecodingStrategy` of the `DateRequest`
-                    let response = HTTPURLResponse(url: URL(string: "http://google.ca")!, statusCode: 200, httpVersion: nil, headerFields: nil)
-                    gettable.dataTaskCompletionHandler(data: data, response: response, error: nil) { response in
-                        if case let .success(tempDateResponse) = response {
-                            dateResponse = tempDateResponse
-                        }
+                let data = """
+                { "date": "\(currentDateString)" }
+                """.data(using: .utf8)!
+                
+                var dateResponse: DateResponse? = nil
+                
+                // NOTE: We use a dummy url response here as the `dataTaskCompletionHandler` needs it.
+                // As long as this method gets a success statusCode (ie. 200) from an HTTPURLResponse, it will proceed to decode the data passed in.
+                // We want to verify that it is decoding it according to the `dateDecodingStrategy` of the `DateRequest`
+                let response = HTTPURLResponse(url: URL(string: "http://google.ca")!, statusCode: 200, httpVersion: nil, headerFields: nil)
+                gettable.dataTaskCompletionHandler(data: data, response: response, error: nil) { response in
+                    if case let .success(tempDateResponse) = response {
+                        dateResponse = tempDateResponse
                     }
-                    
-                    expect(dateResponse).toNot(beNil())
-                    
-                    // compare the dates
-                    let currentComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: currentDate)
-                    let actualComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: dateResponse!.date)
-                    
-                    expect(currentComponents).toNot(beNil())
-                    expect(currentComponents) == actualComponents
-                    return nil
-                }.toNot(throwError())
+                }
+                
+                expect(dateResponse).toNot(beNil())
+                
+                // compare the dates
+                let currentComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: currentDate)
+                let actualComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: dateResponse!.date)
+                
+                expect(currentComponents).toNot(beNil())
+                expect(currentComponents) == actualComponents
             }
         }
     }
